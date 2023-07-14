99a069d0a6f13c8df302e0f2b574b1db0015ba58
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-07-14 16:52:16 +0200

    types folder added. Auth check added.

--- App.tsx
+++ App.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppStack from './src/navigation/AppStack';
 import AuthStack from './src/navigation/AuthStack';
+import {RootState, store} from './src/redux/stores';
+import {Provider, useDispatch, useSelector} from 'react-redux';
 
-const App = () => {
-  const userIsLoggedIn = false;
-
+const Stack = () => {
+  const userIsLoggedIn: boolean = useSelector<RootState, boolean>(
+    state => state.authReducer.isAuthenticated,
+  );
   return (
     <NavigationContainer>
       {userIsLoggedIn ? <AppStack /> : <AuthStack />}
     </NavigationContainer>
+  );
+};
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Stack></Stack>
+    </Provider>
   );
 };
 
