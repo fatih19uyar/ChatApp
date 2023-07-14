04ff4a11938c83302709e10afcf3bfa63d9c0741
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-07-14 17:10:19 +0200

    Spalsh screen added.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppStack from './src/navigation/AppStack';
 import AuthStack from './src/navigation/AuthStack';
 import {RootState, store} from './src/redux/stores';
 import {Provider, useDispatch, useSelector} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = () => {
   const userIsLoggedIn: boolean = useSelector<RootState, boolean>(
@@ -17,9 +18,13 @@ const Stack = () => {
 };
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
-      <Stack></Stack>
+      <Stack />
     </Provider>
   );
 };
--- android/app/src/main/java/com/chatapp/MainActivity.java
+++ android/app/src/main/java/com/chatapp/MainActivity.java
@@ -1,12 +1,8 @@
-package com.chatapp;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen; 
 
 public class MainActivity extends ReactActivity {
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -17,16 +13,21 @@ public class MainActivity extends ReactActivity {
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-   * (aka React 18) with two boolean flags.
+   * Called during onCreate of the main activity to check if a splash screen should be displayed.
    */
   @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this); 
+    super.onCreate(savedInstanceState);
   }
+
+  /**
+   * Returns the name of the bundle in the assets folder.
+   * This is used to provide a custom splash screen.
+   * Uncomment this method if you want to use a custom splash screen.
+   */
+  // @Override
+  // protected String getBundleAssetName() {
+  //   return "splashscreen.bundle";
+  // }
 }
