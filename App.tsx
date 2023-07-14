6fc51eafcd821b8a06d8e982e03b0f2ea2189e46
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2023-07-14 16:12:53 +0200

    Created project.

---
+++ App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Created by OFU
+ * 07.14.23
+ */
+
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+function App(): JSX.Element {
+  return <></>;
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
